8500d858cd1253e85a83bbb0f0a6377b2763cd9d
Christian <christian.hendricks@gmx.de>
2020-02-23 14:34:36 +0100

    Refactor policy network.

--- pi_network.py
+++ pi_network.py
@@ -1,9 +1,7 @@
-import numpy as np
 import torch
 from torch import optim
 import torch.nn as nn
 from collections import OrderedDict
-from torch.distributions.normal import Normal
 import copy
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -11,33 +9,29 @@ device = 'cpu'
 
 
 class PiNetwork(nn.Module):
-    def __init__(self, lr):
+    def __init__(self, input_size, output_size, layer_size1=64, layer_size2=64, lr=1e-5, seed=1337):
         super(PiNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.input_size = input_size
+        self.output_size = output_size
+        self.layer_size1 = layer_size1
+        self.layer_size2 = layer_size2
         self.lr = lr
-        self.model_mu_sigma = nn.Sequential(OrderedDict([
-            ('fc1', nn.Linear(33, 64)),
+
+        self.model = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(input_size, self.layer_size1)),
             ('relu1', nn.ReLU()),
-            ('fc2', nn.Linear(64, 64)),
+            ('fc2', nn.Linear(self.layer_size1, self.layer_size2)),
             ('relu2', nn.ReLU()),
-            ('logits', nn.Linear(64, 4*1)),
+            ('logits', nn.Linear(self.layer_size2, self.output_size)),
             ('tanh', nn.Tanh())
         ]))
-        self.optimizer = optim.Adam(self.model_mu_sigma.parameters(), lr=lr)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
     def forward(self, state):
-        return self.model_mu_sigma.forward(state).reshape(-1, 4).clamp(min=-1.0, max=1.0)
-
-    def act(self, mu_sigma):
-        #state = torch.from_numpy(state).float().to(device)
-        #mu_sigma = self.forward(state)
-        #print("shape", mu_sigma.shape)
-        m = Normal(mu_sigma[:, :4], torch.clamp(0.5+0.5*mu_sigma[:, 4:], min=0.05))
-        actions = m.sample()
-        log_probs = m.log_prob(actions)
-        actions = actions.cpu().data.numpy().reshape(-1, 4)
-        return np.clip(actions, -1, 1)# log_probs.sum(axis=1)
+        return self.model.forward(state).reshape(-1, self.output_size)
 
     def clone(self):
-        pi_network_clone = PiNetwork(self.lr)
-        pi_network_clone.model_mu_sigma = copy.deepcopy(self.model_mu_sigma)
+        pi_network_clone = PiNetwork(self.input_size, self.output_size, self.layer_size1, self.layer_size2, self.lr)
+        pi_network_clone.model = copy.deepcopy(self.model)
         return pi_network_clone
